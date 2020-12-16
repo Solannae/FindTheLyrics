ca59536f57a53f809d2c007dbfc31a238744af4e
Accalia Woirgard <accalia.woirgard@isep.fr>
2020-12-16 11:03:30 +0100

    Main activity

--- app/src/main/java/com/solannae/findthelyrics/MainActivity.java
+++ app/src/main/java/com/solannae/findthelyrics/MainActivity.java
@@ -6,23 +6,52 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import java.io.Serializable;
 
 public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase database;
     private StorageReader reader;
+    private Button Playbutton;
+    private Button LBbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Playbutton = (Button) findViewById(R.id.playbutton);
+        Playbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDeezerLoginActivity();
+            }
+        });
+        LBbutton = (Button) findViewById(R.id.leaderboardbutton);
+        LBbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLeaderBoardActivity();
+            }
+        });
+
         reader = new StorageReader(this);
     }
 
-    public void onClick(View view)
+    /*public void onClick(View view)
     {
         Intent intent = new Intent(this, DeezerLoginActivity.class);
+        startActivity(intent);
+    }*/
+
+    public void openDeezerLoginActivity(){
+        Intent intent = new Intent(this, DeezerLoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void openLeaderBoardActivity(){
+        Intent intent = new Intent(this, LeaderBoardActivity.class);
         startActivity(intent);
     }
 
