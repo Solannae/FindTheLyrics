244375b6bf718f47ab878d0a08a591a7555914a2
cwy <nanashisan743@outlook.com>
2021-01-20 15:51:31 +0100

    GameActivity update + youtube configuration

--- app/src/main/java/com/solannae/findthelyrics/GameActivity.java
+++ app/src/main/java/com/solannae/findthelyrics/GameActivity.java
@@ -2,13 +2,129 @@ package com.solannae.findthelyrics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class GameActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class GameActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
+    private MyPlaybackEventListener playbackEventListener;
+    private MyPlayerStateChangeListener playerStateChangeListener;
+    private String videoId = "0RU_05zpETo";
+    private YouTubePlayer ytPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+
+        playerStateChangeListener = new MyPlayerStateChangeListener();
+        playbackEventListener = new MyPlaybackEventListener();
+
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        //getFirstVideoId("The Weeknd Blinding Lights");
+        //videoId = "Y0EcKR05Ac4";
+        youTubeView.initialize(YTConfig.YOUTUBE_API_KEY, (YouTubePlayer.OnInitializedListener) this);
+
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.setPlayerStateChangeListener(playerStateChangeListener);
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
+        // youTubePlayer.setPlayerStyle(YouTubePlayer.PlayerStyle.CHROMELESS);
+        this.ytPlayer = youTubePlayer;
+
+        if (!b) {
+            getFirstVideoId("The Weeknd Blinding Lights");
+            System.out.println("------------");
+            System.out.println("current vid: "+videoId);
+            System.out.println(ytPlayer==null);
+            ytPlayer.cueVideo(videoId);
+
+
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(String.valueOf(R.string.player_error), youTubeInitializationResult.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            youTubeView.initialize(YTConfig.YOUTUBE_API_KEY, this);
+        }
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+
+    public void getFirstVideoId(String keyword){
+        String url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q="+keyword+"&type=video&key="+YTConfig.YOUTUBE_API_KEY;
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new
+                Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.d("Volley response", response.toString());
+                            //JSONObject items = response.getJSONObject("items");
+                            JSONArray items = response.getJSONArray("items");
+                            //JSONObject id = response.getJSONObject("id");
+                            JSONObject jo = items.getJSONObject(0);
+                            JSONObject id = jo.getJSONObject("id");
+                            String vId = id.getString("videoId");
+                            System.out.println(vId);
+                            videoId = vId;
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Volley response", "An error occurred.");
+            }
+        });
+        queue.add(request);
+
+
+
     }
 }
